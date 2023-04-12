5caa0f79b7abf8f7bcc581f297984b11c2427cb0
Sriharsha Dokula <sdokula1@cs.binghamton.edu>
2023-04-12 18:03:06 -0400

    char count done

--- hauffman/hauffman.c
+++ hauffman/hauffman.c
@@ -12,20 +12,26 @@ int main(int argc, char * argv[])
 
 
     FILE* file_in = fopen(argv[1],"r");
-    char word[1024];
-    char 
+    int symbol_count[256] = {0};
+
     if (file_in == NULL)
     {
         printf("file can't be opened \n");
         exit(1);
     }
 
-    while(fgets(word,1024,file_in) != NULL)
-    {
-	//TODO: count occurences of each character in the file.	
-	
+    int symbol = fgetc(file_in);
+    while((char)symbol != EOF)
+    {	
+	    //printf("/%c/",(char)symbol);
+	    symbol_count[symbol]++;
+	    symbol = fgetc(file_in);
     }
 
+    printf("char count of A:%d\n",symbol_count[65]);
+    printf("char count of 5:%d\n",symbol_count[53]);
+    printf("char count of @:%d\n",symbol_count[64]);
+    printf("char count of }:%d\n",symbol_count[125]);
     fclose(file_in);
     return 0;
 }
